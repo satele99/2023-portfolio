af1c75e2a79e6294d2016127067b3be46ba8ae6d
satele99 <satele.works@gmail.com>
2023-05-23 20:33:06 -0400

    working on image/grid layout

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,5 +1,19 @@
 const toggleBun = document.getElementById('toggle')
 
 toggleBun.addEventListener('click', ()=> {
-    
+    const dropdown = document.querySelector('.nav_dropdown');
+    dropdown.classList.toggle('open')
+    const isOpen = dropdown.classList.contains('open')
+
+    toggleBun.classList = isOpen ? 'bi bi-x-lg' : 'bi bi-list';
 })
+
+// const respToggle = () => {
+//     const toggle = document.querySelector('.toggle-btn');
+//     const toggleIcon = document.querySelector('.toggle-btn i');
+//     const dropdown = document.querySelector('.dropdown');
+//     dropdown.classList.toggle('open')
+//     const isOpen = dropdown.classList.contains('open')
+
+//     toggleIcon.classList = isOpen ? 'bi bi-x-lg' : 'bi bi-list';
+// }
