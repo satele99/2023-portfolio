ea7f230b68bde7a613043123d221ff7c6e3f3cd7
satele99 <satele.works@gmail.com>
2023-05-24 12:19:43 -0400

    contact form created/downloadable resume link added

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -8,12 +8,6 @@ toggleBun.addEventListener('click', ()=> {
     toggleBun.classList = isOpen ? 'bi bi-x-lg' : 'bi bi-list';
 })
 
-// const respToggle = () => {
-//     const toggle = document.querySelector('.toggle-btn');
-//     const toggleIcon = document.querySelector('.toggle-btn i');
-//     const dropdown = document.querySelector('.dropdown');
-//     dropdown.classList.toggle('open')
-//     const isOpen = dropdown.classList.contains('open')
-
-//     toggleIcon.classList = isOpen ? 'bi bi-x-lg' : 'bi bi-list';
-// }
+function copyEmail() {
+    navigator.clipboard.writeText('satele.works@gmail.com')
+}
